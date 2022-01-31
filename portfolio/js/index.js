1924f9339c5cc532f4e755da461794d79167c2b1
Nika Perfilova <ees2009.2014@gmail.com>
2022-01-31 21:08:28 +0700

    feat: add background for fixed header

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -29,6 +29,15 @@ const getLocalStorage = () => {
 window.addEventListener('load', getLocalStorage);
 window.addEventListener('beforeunload', setLocalStorage);
 
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    header.classList.add('header--fixed');
+  } else {
+    header.classList.remove('header--fixed');
+  }
+});
+
 const navToggle = document.querySelector('.header__nav-toggle');
 navToggle.addEventListener('click', toggleNav);
 
