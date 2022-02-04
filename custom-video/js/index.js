dc00e54b2703444fe25d2a1c571c75b885f5ecde
Nika Perfilova <ees2009.2014@gmail.com>
2022-02-04 11:09:19 +0700

    feat: add volume controls

--- custom-video/js/index.js
+++ custom-video/js/index.js
@@ -1,10 +1,13 @@
 const player = document.querySelector('.video-player');
 const video = player.querySelector('.video-player__video');
-const playButton = player.querySelector('.video-player__button--play');
+const playButton = player.querySelector('.video-player__play-btn');
 const progress = player.querySelector('.video-player__progress');
 const progressBar = player.querySelector('.video-player__progress-filled');
+const volumeButton = player.querySelector('.video-player__volume-btn');
+const volumeRange = player.querySelector('.video-player__volume-range');
 
 const toggleVideoPlay = () => (video.paused ? video.play() : video.pause());
+const toggleVideoMute = () => (video.muted = !video.muted);
 
 const updatePlayer = () => {
   if (video.paused) {
@@ -13,6 +16,23 @@ const updatePlayer = () => {
   } else {
     player.classList.add('video-player--playing');
     playButton.title = 'Pause';
+  }
+};
+
+const updateVolumeBtn = () => {
+  if (video.volume && !video.muted) {
+    volumeButton.classList.remove('video-player__volume-btn--muted');
+    volumeButton.title = 'Mute';
+  } else {
+    volumeButton.classList.add('video-player__volume-btn--muted');
+    volumeButton.title = 'Unmute';
+  }
+};
+
+const handleVolumeRangeUpdate = (e) => {
+  video.volume = e.target.value;
+  if (e.target.value) {
+    video.muted = false;
   }
 };
 
@@ -29,11 +49,19 @@ const progressLoop = () => {
 video.addEventListener('click', toggleVideoPlay);
 video.addEventListener('play', updatePlayer);
 video.addEventListener('pause', updatePlayer);
+video.addEventListener('play', progressLoop);
+video.addEventListener('volumechange', updateVolumeBtn);
 playButton.addEventListener('click', toggleVideoPlay);
+volumeButton.addEventListener('click', toggleVideoMute);
 
-video.addEventListener('play', () => requestAnimationFrame(progressLoop));
-let mousedown = false;
+let volumeMousedown = false;
+volumeRange.addEventListener('change', handleVolumeRangeUpdate);
+volumeRange.addEventListener('mousemove', (e) => volumeMousedown && handleVolumeRangeUpdate(e));
+volumeRange.addEventListener('mouseup', () => (volumeMousedown = false));
+volumeRange.addEventListener('mousedown', () => (volumeMousedown = true));
+
+let progressMousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mouseup', () => (mousedown = false));
-progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mousemove', (e) => progressMousedown && scrub(e));
+progress.addEventListener('mouseup', () => (progressMousedown = false));
+progress.addEventListener('mousedown', () => (progressMousedown = true));
